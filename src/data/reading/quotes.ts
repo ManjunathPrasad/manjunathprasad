220de6b897463fe98c77fe148f7fd035a042ac8b
manjunathprasad <manjunathprasad99@gmail.com>
2026-06-18 15:07:46 +0530

    Added quotes

--- src/data/reading/quotes.ts
+++ src/data/reading/quotes.ts
@@ -2,28 +2,41 @@ import type { Quote } from "../../types/Quote";
 
 export const quotes: Quote[] = [
 
-    {
-        id: 1,
-        text:
-            "The struggle itself toward the heights is enough to fill a man's heart.",
-        source: "The Myth of Sisyphus",
-        author: "Albert Camus"
-    },
+    // {
+    //     id: 1,
+    //     text:
+    //         "The struggle itself toward the heights is enough to fill a man's heart.",
+    //     source: "The Myth of Sisyphus",
+    //     author: "Albert Camus"
+    // },
+
+    // {
+    //     id: 2,
+    //     text:
+    //         "The mystery of human existence lies not in just staying alive, but in finding something to live for.",
+    //     source: "The Brothers Karamazov",
+    //     author: "Fyodor Dostoevsky"
+    // },
+
+    // {
+    //     id: 3,
+    //     text:
+    //         "I am free and that is why I am lost.",
+    //     source: "Kafka",
+    //     author: "Franz Kafka"
+    // },
 
     {
-        id: 2,
-        text:
-            "The mystery of human existence lies not in just staying alive, but in finding something to live for.",
-        source: "The Brothers Karamazov",
-        author: "Fyodor Dostoevsky"
+        id: 1 ,
+        text:"ಬಾಳು ಚಿರವಲ್ಲದಿರೆ, ಭೂಮಿ ಚಿರವಲ್ಲದಿರೆ, ಜೀವದಾನಂದವಿದು ಚಿರವಲ್ಲವೇನು?",
+        source:"ಜಲಗಾರ",
+        author:"ಕುವೆಂಪು"
     },
-
     {
-        id: 3,
-        text:
-            "I am free and that is why I am lost.",
-        source: "Kafka",
-        author: "Franz Kafka"
-    }
+        id: 2 ,
+        text:"ಮನುಷ್ಯನನ್ನು ಹೆಚ್ಚುಕಾಲ ನೋಡಿದಷ್ಟೂ ಪ್ರಾಣಿಪಕ್ಷಿಗಳ ಬಗ್ಗೆ ಪ್ರೀತಿ ಜಾಸ್ತಿಯಾಗುತ್ತದೆ",
+        source:"ಟೀಕೆ-ಟಿಪ್ಪಣಿ ಸಂಪುಟ 3 ",
+        author:"ಪಿ. ಲಂಕೇಶ್"
+    },
 
 ];
